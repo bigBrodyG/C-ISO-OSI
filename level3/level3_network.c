83b95ad5f55887375a4ba1fb9f9ae3aa6b3114fa
Franciuto <francesco.traversetolo@gmail.com>
2025-05-27 17:28:40 +0200

    Add header for level3

--- level3/level3_network.c
+++ level3/level3_network.c
@@ -14,39 +14,20 @@ char *livello3_send(const char *dati)
 
 char *livello3_receive(const char *pdu) {
     char *data_from_l2 = livello2_receive(pdu);
-
-    const char *header = "[SRC=192.168.1.7] [DST=192.168.1.45]";
+    char *forward_datas = calloc(100, 1);
+    const char *header = "[SRC=192.168.1.7] [DST=192.168.1.45] ";
 
     if (data_from_l2 == NULL) {
         printf("[L3] Network - No data received from L2.\n");
         return NULL; // Propagate the NULL up the stack
     }
-
+    
+    strcat(forward_datas, header);
+    strcat(forward_datas, data_from_l2);
     //printf("[L3] Network - Received PDU from L2: \"%s\"\n", data_from_l2);
-
-    printf("[L3] Network - Sending data to L4: \"%s\"\n", data_from_l2);
+    // data_forward = strdup(data_from_l2); // Removed undefined variable usage
+    
+    printf("[L3] Network - Sending data to L4: \"%s\"\n", forward_datas);
 
     return data_from_l2;
 }
-/*
-char *livello3_receive(const char *pdu)
-{
-
-    // const char* header = "[SRC=192.168.1.2] [DST=192.168.1.5]";
-    char datas = calloc(100, 1);
-    strcat(*datas, livello2_receive(pdu));
-
-    char* src[50] = {'\0'};
-    strcat(*src, livello2_receive(pdu));
-    printf("[L3: Sending to L4] %s", src);
-    return *src;
-
-   printf("\n\n%s\n\n", datas);
-   return *datas;
-}
-*/
-/*
-char* livello3_receive(const char* pdu) {
-    return livello2_receive(pdu);
-}
-*/
