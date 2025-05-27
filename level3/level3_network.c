f5f3fe51be8358b077099b90745c89970820129e
Giordi <gfornari.casa@gmail.com>
2025-05-27 17:18:54 +0200

    Refactor network layer functions and remove merge conflict markers

--- level3/level3_network.c
+++ level3/level3_network.c
@@ -35,3 +35,24 @@ char *livello3_receive(const char *pdu) {
 
     return data_for_l4;
 }
+/*
+char *livello3_receive(const char *pdu)
+{
+
+    // const char* header = "[SRC=192.168.1.2] [DST=192.168.1.5]";
+    char datas = calloc(100, 1);
+    strcat(*datas, livello2_receive(pdu));
+
+    char* src[50] = {'\0'};
+    strcat(*src, livello2_receive(pdu));
+    printf("[L3: Sending to L4] %s", src);
+    return *src;
+
+   printf("\n\n%s\n\n", datas);
+   return *datas;
+}
+*/
+
+char* livello3_receive(const char* pdu) {
+    return livello2_receive(pdu);
+}
