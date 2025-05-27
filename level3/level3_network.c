8992b6f9d5b44c321208e4bb160421b1892b9e45
Franciuto <francesco.traversetolo@gmail.com>
2025-05-27 16:57:40 +0200

    Bufferize

--- level3/level3_network.c
+++ level3/level3_network.c
@@ -1,11 +1,34 @@
 /* C-ISO-OSI - Network layer - Functions */
-#include "constants.h"  // Library constants
+#include "constants.h" // Library constants
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "level3_network.h"
 #include "level2_datalink.h"
 
-char* livello3_send(const char* dati) {
+char *livello3_send(const char *dati)
+{
+    printf("[L3] Ricevuto da livello 2: %s\n", dati);
     return livello2_send(dati);
 }
+
+char *livello3_receive(const char *pdu)
+{
+
+    // const char* header = "[SRC=192.168.1.2] [DST=192.168.1.5]";
+    char datas = calloc(100, 1);
+    strcat(*datas, livello2_receive(pdu));
+    /*
+    char* src[50] = {'\0'};
+    strcat(*src, livello2_receive(pdu));
+    printf("[L3: Sending to L4] %s", src);
+    return *src;
+    */
+   printf("\n\n%s\n\n", datas);
+   return *datas;
+}
+
+/*
 char* livello3_receive(const char* pdu) {
     return livello2_receive(pdu);
-}
+}*/
